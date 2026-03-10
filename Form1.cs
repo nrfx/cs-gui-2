ee7623b6b4bf56b3bbe619e41e19f2b1df4ad122
nrfx <zavovaz001@gmail.com>
2026-03-10 22:37:17 +0800

    сделал все но не работает сохранение при выходе

--- Form1.cs
+++ Form1.cs
@@ -5,11 +5,58 @@ namespace cs_gui_2
         public Form1()
         {
             InitializeComponent();
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int a, b, c;
 
+            try
+            {
+                a = int.Parse(this.textBoxA.Text);
+                b = int.Parse(this.textBoxB.Text);
+                c = int.Parse(this.textBoxC.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("введите корректные числа");
+                return;
+            }
+
+            Properties.Settings.Default.saveA = textBoxA.Text;
+            Properties.Settings.Default.saveB = textBoxB.Text;
+            Properties.Settings.Default.saveC = textBoxC.Text;
+            Properties.Settings.Default.Save();
+
+            int res = CalculateProduct.Calculate(a, b, c);
+
+            MessageBox.Show($"Произведение двух меньших чисел: {res}");
         }
     }
+}
+
+public class CalculateProduct
+{
+    public static int Calculate(int a, int b, int c)
+    {
+        int res = 0;
+        if (a > b && a > c)
+        {
+            res = b * c;
+            return res;
+        }
+        else if (b > a && b > c)
+        {
+            res = a * c;
+            return res;
+        }
+        else
+        {
+            res = a * b;
+            return res;
+        }
+    }
+
 }
